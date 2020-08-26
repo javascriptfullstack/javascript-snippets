1d46016548f9850e5a8fcf49bba0169fdd0e56b3
Stephen Sun <stephensun.me@gmail.com>
2020-08-26 18:52:50 -0500

    add code snippet: how to get all the keys and values in an object

---
+++ how-to-get-all-the-keys-and-values-in-an-object/index.js
@@ -0,0 +1,27 @@
+// How to get all the keys and values in an object
+
+// Start with an object
+const person = {
+  firstName: 'John',
+  lastName: 'Doe',
+  age: 25,
+}
+
+// Get keys
+// Returns an array
+const keys = Object.keys(person)
+console.log(keys) // ['firstName', 'lastName', 'age']
+
+// Get values
+// Returns an array
+const values = Object.values(person)
+console.log(values) // ['John', 'Doe', 25]
+
+// Get keys and values
+// Returns an array of arrays
+const entries = Object.entries(person)
+console.log(entries) // [['firstName', 'John'], ['lastName', 'Doe'], ['age', 25]]
+
+// Get original object back
+const obj = Object.fromEntries(entries)
+console.log(obj) // { firstName: 'John', lastName: 'Doe', age: '25 }
