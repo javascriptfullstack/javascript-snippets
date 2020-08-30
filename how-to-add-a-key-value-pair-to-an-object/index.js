4f579951e710d849059ebd3f2e3345cdcee24c20
Stephen Sun <stephensun.me@gmail.com>
2020-08-30 11:43:12 -0500

    add code snippet: how to add a key/value pair to an object

---
+++ how-to-add-a-key-value-pair-to-an-object/index.js
@@ -0,0 +1,15 @@
+// How to add a key/value pair to an object
+
+// Start with an object
+const orange = {
+  type: 'fruit',
+  color: 'orange',
+}
+
+// Method 1: Dot notation
+orange.shaipe = 'circle'
+
+// Method 2: Square bracket notation
+orange['weight'] = 4
+
+console.log(orange) // { type: 'fruit', color: 'orange', shaipe: 'circle', weight: 4 }
