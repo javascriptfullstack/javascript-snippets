ecc5a237e972e84ff869b5293aa3dc43004d312a
Stephen Sun <stephensun.me@gmail.com>
2020-08-31 08:36:42 -0500

    add log outputs

--- how-to-remove-whitespace-from-a-string/index.js
+++ how-to-remove-whitespace-from-a-string/index.js
@@ -5,8 +5,8 @@ const textWithSpace = '  JavaScript Full Stack '
 
 // Method 1: Built-in trim() method
 const trimmed = textWithSpace.trim()
-console.log(trimmed)
+console.log(trimmed) // 'JavaScript Full Stack'
 
 // Method 2: Regex
 const regex = textWithSpace.replace(/^\s+|\s+$/g, '')
-console.log(regex)
+console.log(regex) // 'JavaScript Full Stack'
