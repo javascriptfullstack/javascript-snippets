c17a30040b38b9e29975220cf9fffca30c341bfb
Stephen Sun <stephensun.me@gmail.com>
2020-08-31 15:13:09 -0500

    add code snippet: how to convert a string to all lowercase or uppercase

---
+++ how-to-convert-a-string-to-all-uppercase-or-lowercase/index.js
@@ -0,0 +1,12 @@
+// How to convert a string to all lowercase or uppercase
+
+// Start with a string
+const text = 'JavaScript Full Stack'
+
+// Convert to all uppercase
+const uppercase = text.toUpperCase()
+console.log(uppercase) // JAVASCRIPT FULL STACK
+
+// Convert to all lowercase
+const lowercase = text.toLowerCase()
+console.log(lowercase) // javascript full stack
