73ac1b703eef343c40dfd657ad19f969efd4b5e8
Stephen Sun <stephensun.me@gmail.com>
2020-09-04 09:03:59 -0500

    add log outputs

--- how-to-remove-null-or-undefined-values-in-an-array/index.js
+++ how-to-remove-null-or-undefined-values-in-an-array/index.js
@@ -6,9 +6,9 @@ const array = ['JavaScript', 'Full', 'Stack', undefined, 3, null, 37]
 // Method 1: Boolean constructor
 // Returns a new array
 const method1 = array.filter(Boolean)
-console.log(method1)
+console.log(method1) // [ 'JavaScript', 'Full', 'Stack', 3, 37 ]
 
 // Method 2: Check for null and undefined values
 // Returns a new array
 const method2 = array.filter(item => item !== null && item !== undefined)
-console.log(method2)
+console.log(method2) // [ 'JavaScript', 'Full', 'Stack', 3, 37 ]
