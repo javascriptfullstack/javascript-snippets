4062d41971a151077e08dbfbb43074886f144c17
Stephen Sun <stephensun.me@gmail.com>
2020-09-04 14:55:26 -0500

    add console log outputs

--- how-to-get-the-last-item-in-an-array/index.js
+++ how-to-get-the-last-item-in-an-array/index.js
@@ -5,17 +5,17 @@ const values = [1, 2, 3, 4, 5]
 
 // Method 1: Array.length()
 const method1 = values[values.length - 1]
-console.log(method1)
+console.log(method1) // 5
 
 // Method 2: Array.slice()
 const method2 = values.slice(-1)[0]
-console.log(method2)
+console.log(method2) // 5
 
 // Method 3: Array.slice() with destructuring
 const [method3] = values.slice(-1)
-console.log(method3)
+console.log(method3) // 5
 
 // Method 4: Array.pop()
 // ** This method removes the element from the array
 const method4 = values.pop()
-console.log(method4)
+console.log(method4) // 5
