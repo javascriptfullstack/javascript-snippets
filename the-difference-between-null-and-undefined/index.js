b428231bfea32e35d5cee2283a3765b80ccb55e2
Stephen Sun <stephensun.me@gmail.com>
2020-09-07 16:25:19 -0500

    add code snippet: the difference between null and undefined

---
+++ the-difference-between-null-and-undefined/index.js
@@ -0,0 +1,12 @@
+// The difference between null and undefined
+
+// A variable is undefined if it's been declared but has not been assigned a value
+let value1
+console.log(value1) // undefined
+console.log(typeof value1) // undefined
+
+// Null itself is a value and can be assigned to variables
+// Null means no value
+let value2 = null
+console.log(value2) // null
+console.log(typeof value2) // object
