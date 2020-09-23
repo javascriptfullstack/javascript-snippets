cce58f7e233d8fd441d017a61b036e476eb4f34b
Stephen Sun <stephensun.me@gmail.com>
2020-09-23 09:02:05 -0500

    add code snippet: how to get the last number of characters of a string

---
+++ how-to-get-the-last-number-of-characters-of-a-string/index.js
@@ -0,0 +1,15 @@
+// How to get the last number of characters of a string
+
+// Start with a string
+const name = 'JavaScript Full Stack'
+
+// Method 1: split() + pop()
+// Splits the string into an array on the spaces and gets the last element of the array.
+const method1 = name.split(' ').pop()
+console.log(method1) // Stack
+
+// Method 2: slice()
+// Use the length of the string and subtract the number of characters we want to get.
+// This gets our starting point and no second argument for slice() gets the rest of the characters.
+const method2 = name.slice(name.length - 5)
+console.log(method2) // Stack
