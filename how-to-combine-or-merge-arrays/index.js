7a7f239f2ea4a62be6503a341288d9f7e11d6648
Stephen Sun <stephensun.me@gmail.com>
2021-10-24 19:25:41 -0500

    update snippet for "how to combine or merge arrays"

--- how-to-combine-or-merge-arrays/index.js
+++ how-to-combine-or-merge-arrays/index.js
@@ -1,23 +1,21 @@
-// How to combine or merge arrays
-
-// Test arrays
+// Example arrays
 const numbers = [1, 2, 3]
 const colors = ['red', 'blue', 'green']
 
 // Method 1: Concatenate
-const concat = [].concat(numbers, colors)
-console.log(concat) // [ 1, 2, 3, 'red', 'blue', 'green' ]
+const concatMethod = [].concat(numbers, colors)
+console.log(concatMethod) // [ 1, 2, 3, 'red', 'blue', 'green' ]
 
 // Method 2: Spread operator
-const spread = [...numbers, ...colors]
-console.log(spread) // [ 1, 2, 3, 'red', 'blue', 'green' ]
+const spreadMethod = [...numbers, ...colors]
+console.log(spreadMethod) // [ 1, 2, 3, 'red', 'blue', 'green' ]
 
 // Method 3: Reduce
 const arrays = [numbers, colors]
-const newArray = arrays =>
+const reduceMethod = arrays =>
   arrays.reduce((accumulator, next) => [...accumulator, ...next])
-console.log(newArray(arrays)) // [ 1, 2, 3, 'red', 'blue', 'green' ]
+console.log(reduceMethod(arrays)) // [ 1, 2, 3, 'red', 'blue', 'green' ]
 
 // Method 4: Flat
-const flat = [numbers, colors].flat()
-console.log(flat) // [ 1, 2, 3, 'red', 'blue', 'green' ]
+const flatMethod = [numbers, colors].flat()
+console.log(flatMethod) // [ 1, 2, 3, 'red', 'blue', 'green' ]
